f51b6e4d859701f2cf6ae4eb962eeca46224c857
xiezhiyan <xiezhiyanfighting@163.com>
2020-04-08 17:32:16 +0800

    *

--- ios/iTablet/AppDelegate.m
+++ ios/iTablet/AppDelegate.m
@@ -93,7 +93,7 @@ static NSString* g_sampleCodeName = @"#";;
   
 #if DEBUG
 
-  [[RCTBundleURLProvider sharedSettings] setJsLocation:@"192.168.43.238"];
+  [[RCTBundleURLProvider sharedSettings] setJsLocation:@"192.168.43.32"];
 
 #endif
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
@@ -312,6 +312,6 @@ void initCrash(){
 //  } else {
 //    return UIInterfaceOrientationMaskPortrait;
 //  }
-  return [Orientation getOrientation];
+  return UIInterfaceOrientationMaskAll;
 }
 @end

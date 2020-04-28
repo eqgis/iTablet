10ea2e280a7cf9d619490f1c1973ea1451c0245c
xiezhiyan <xiezhiyanfighting@163.com>
2020-04-28 20:05:23 +0800

    Update MapExample.js

--- configs/mapModules/MapExample.js
+++ configs/mapModules/MapExample.js
@@ -8,23 +8,23 @@ import {
   toolModule,
   shareModule,
 } from '../../src/containers/workspace/components/ToolBar/modules'
-// import { functionExample } from '../mapFunctionModules'
+import { functionExample } from '../mapFunctionModules'
 
 export default class MapExample extends Module {
   constructor () {
     super({
       key: 'MapExample',
-      example: {
-        name_en: 'PrecipitationOfUSA', // 英文数据名称
-        name_cn: '湖北', // 中文数据名称
-      },
+      // example: {
+      //   name_en: 'PrecipitationOfUSA', // 英文数据名称
+      //   name_cn: '湖北', // 中文数据名称
+      // },
       functionModules: [
         startModule(),
-        addModule(),
+        // addModule(),
         markModule(),
         toolModule(),
         shareModule(),
-        // functionExample(),
+        functionExample(),
       ],
       // tabModules: [mapTabModules.MapView, mapTabModules.LayerManager, mapTabModules.LayerAttribute, mapTabModules.MapSetting]
     })
@@ -37,9 +37,9 @@ export default class MapExample extends Module {
       // 根据语言获取地图模块名称
       title: language === 'CN' ? '示例模块' : 'Example',
       // 模块图片
-      moduleImage: getThemeAssets().nav.icon_map_theme,
+      moduleImage: require('../../src/assets/userDefine/userDefineTab.png'),
       // 点击时模块高亮图片
-      moduleImageTouch: getThemeAssets().nav.icon_map_theme_touch,
+      moduleImageTouch: require('../../src/assets/userDefine/userDefineTab.png'),
       // 默认地图名称
       defaultMapName: GLOBAL.language === 'CN' ? 'LandBuild' : 'PrecipitationOfUSA',
       // 地图默认底图数据

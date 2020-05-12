58184487b310078a2c951b88ce0a16dcc0968f3f
xiezhiyan <xiezhiyanfighting@163.com>
2020-05-12 11:40:56 +0800

    tcw mapController显隐问题修改

--- src/containers/workspace/pages/mapView/MapView.js
+++ src/containers/workspace/pages/mapView/MapView.js
@@ -2027,7 +2027,7 @@ export default class MapView extends React.Component {
 
   /** 地图控制器，放大缩小等功能 **/
   renderMapController = () => {
-    if (this.state.currentFloorID && this.mapController?.state.isGuiding)
+    if (this.state.currentFloorID || this.mapController?.state.isGuiding)
       return null
     return (
       <MapController

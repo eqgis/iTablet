c2b8ea451ab1e8b3d812333db7c1be5f0a584e15
DragonYang <dragonyang0917@gmail.com>
2020-06-24 11:19:44 +0800

    修复三维地图界面下载示例数据进度条横竖屏切换高度
    作者：yangsl
    本领域专家：xiezy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/containers/workspace/pages/map3D/Map3D.js
+++ src/containers/workspace/pages/map3D/Map3D.js
@@ -885,10 +885,11 @@ export default class Map3D extends React.Component {
     return (
       <Progress
         ref={ref => (this.mProgress = ref)}
-        style={[styles.progressView, {
-          height: Platform.OS === 'ios' &&
-          this.props.device.orientation.indexOf('PORTRAIT') === 0 ? 20 : 8,
-        }]}
+        style={styles.progressView}
+        height={
+          Platform.OS === 'ios' &&
+          this.props.device.orientation.indexOf('PORTRAIT') === 0 ? 20 : 8
+        }
         progressAniDuration={0}
         progressColor={color.item_selected_bg}
       />

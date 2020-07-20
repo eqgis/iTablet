2288617c84a9eeddd6121f8e7c661b369d529bf3
DragonYang <dragonyang0917@gmail.com>
2020-07-20 14:45:54 +0800

    适配RedMi等机型Text显示问题
    作者：yangsl
    本领域专家：xiezy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

---
+++ src/customPrototype/TextPrototype.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import { Text, Platform } from 'react-native'
+
+const sourceRender = Text.render
+Text.render = function render(props, ref) {
+  return sourceRender.apply(this, [{ ...props, style: [Platform.OS === 'android' && { fontFamily: '' }, props.style] }, ref])
+}
--- src/customPrototype/index.js
+++ src/customPrototype/index.js
@@ -1,6 +1,7 @@
 import MapPrototype from './MapPrototype'
 import DatePrototype from './DatePrototype'
 import ArrayPrototype from './ArrayPrototype'
+import TextPrototype from './TextPrototype'
 
 function initCustomPrototype() {
   MapPrototype()

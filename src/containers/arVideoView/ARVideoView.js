d86d5fee67f73c39189ce75bc671abc840bbbaba
xiezhiyan <xiezhiyanfighting@163.com>
2020-07-30 17:44:29 +0800

    添加AR视屏修改，删除功能
    作者：zhangxt
    同行：yangsl
    本领域专家：xiezhy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/containers/arVideoView/ARVideoView.js
+++ src/containers/arVideoView/ARVideoView.js
@@ -1,11 +1,9 @@
 import * as React from 'react'
 import { SMARVideoView, SARVideoView } from 'imobile_for_reactnative'
 import Orientation from 'react-native-orientation'
-import { Container, ImagePicker } from '../../components'
-import { scaleSize } from '../../utils'
-import { View, TouchableOpacity, Image } from 'react-native'
-import { getPublicAssets } from '../../assets'
+import { Container, BottomBar } from '../../components'
 import { getLanguage } from '../../language'
+import MenuData from './MenuData'
 
 export default class ARVideoView extends React.Component {
   props: {
@@ -37,46 +35,7 @@ export default class ARVideoView extends React.Component {
   componentWillUnmount() {}
 
   renderBottom = () => {
-    return (
-      <View
-        style={{
-          position: 'absolute',
-          left: 0,
-          bottom: 0,
-          width: '100%',
-          height: scaleSize(80),
-          backgroundColor: 'black',
-          justifyContent: 'center',
-        }}
-      >
-        <TouchableOpacity
-          style={{
-            flexDirection: 'row',
-            marginLeft: scaleSize(40),
-            alignItems: 'center',
-          }}
-          onPress={() => {
-            ImagePicker.AlbumListView.defaultProps.showDialog = false
-            ImagePicker.AlbumListView.defaultProps.assetType = 'Videos'
-            ImagePicker.AlbumListView.defaultProps.groupTypes = 'All'
-            ImagePicker.getAlbum({
-              maxSize: 1,
-              callback: async data => {
-                if (data && data.length > 0) {
-                  let path = data[0].uri
-                  SARVideoView.addVideoAtCurrentPosition(path)
-                }
-              },
-            })
-          }}
-        >
-          <Image
-            source={getPublicAssets().common.icon_album}
-            style={{ width: scaleSize(60), height: scaleSize(60) }}
-          />
-        </TouchableOpacity>
-      </View>
-    )
+    return <BottomBar getData={MenuData.getPage} />
   }
 
   render() {

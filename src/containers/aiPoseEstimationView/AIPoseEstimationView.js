aa893c8cd053bf2981ac8bf2ef2cb4f2356695eb
xiezhiyan <xiezhiyanfighting@163.com>
2020-08-07 08:49:07 +0800

    修改人体姿态界面
    修改人体姿态界面
    作者：zhouyuming
    同行：yangshanglong
    本领域专家：xiezhiyan
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/containers/aiPoseEstimationView/AIPoseEstimationView.js
+++ src/containers/aiPoseEstimationView/AIPoseEstimationView.js
@@ -13,12 +13,15 @@ import {
   Text,
   DeviceEventEmitter,
   Platform,
+  FlatList,
 } from 'react-native'
 // import { getPublicAssets } from '../../assets'
 import { getLanguage } from '../../language'
 import NavigationService from '../../containers/NavigationService'
 import styles from './styles'
 import { getThemeAssets, getPublicAssets } from '../../assets'
+import { color } from '../../styles'
+import { scaleSize } from '../../utils'
 
 export default class AIPoseEstimationView extends React.Component {
   props: {
@@ -37,6 +40,10 @@ export default class AIPoseEstimationView extends React.Component {
       associationMap: false, //关联地图
       poseOverLook: false, //忽略姿态
       poseTitle: '', //姿态标题
+
+      poseData: this.poseData,
+      poseMode: 'MODE_PAN', //默认漫游姿态
+      selectPoseMode: false, //是否是选择姿态模式
     }
   }
 
@@ -124,6 +131,7 @@ export default class AIPoseEstimationView extends React.Component {
               SPoseEstimationView.setPoseOverlook(!this.state.poseOverLook)
               this.setState({
                 poseOverLook: !this.state.poseOverLook,
+                // selectPoseMode: !this.state.selectPoseMode,
               })
             }}
             style={{
@@ -194,25 +202,149 @@ export default class AIPoseEstimationView extends React.Component {
     )
   }
 
+  poseData = [
+    {
+      title: getLanguage(global.language).Map_Main_Menu
+        .MAP_AI_POSE_ESTIMATION_ZOOM,
+      mode: 'MODE_ZOOM',
+      index: 0,
+    },
+    {
+      title: getLanguage(global.language).Map_Main_Menu
+        .MAP_AI_POSE_ESTIMATION_PAN,
+      mode: 'MODE_PAN',
+      index: 1,
+    },
+    {
+      title: getLanguage(global.language).Map_Main_Menu
+        .MAP_AI_POSE_ESTIMATION_OVERLOOK,
+      mode: 'MODE_OVERLOOK',
+      index: 2,
+    },
+  ]
+  onPressItem = item => {
+    SPoseEstimationView.setOperMode(item.mode)
+    this.setState({
+      poseMode: item.mode,
+    })
+  }
+
+  renderItem = ({ item }) => {
+    return (
+      <View style={styles.itemView}>
+        <TouchableOpacity
+          onPress={() => {
+            this.onPressItem(item)
+          }}
+        >
+          <Image
+            style={styles.image}
+            source={
+              item.poseMode === this.state.poseMode
+                ? getPublicAssets().common.icon_check
+                : getPublicAssets().common.icon_uncheck
+            }
+          />
+        </TouchableOpacity>
+        <Text style={styles.text}>{item.title}</Text>
+      </View>
+    )
+  }
+  _keyExtractor = item => item.title + item.type
+
+  _renderItemSeparatorComponent = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '100%',
+          backgroundColor: color.gray5,
+        }}
+      />
+    )
+  }
+
+  renderModelList = () => {
+    return (
+      <View
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            backgroundColor: color.transOverlay,
+            height: '60%',
+          }}
+          onPress={() => {
+            this.setState({
+              selectPoseMode: false,
+            })
+          }}
+        />
+        <View
+          style={{
+            backgroundColor: color.white,
+            height: scaleSize(480),
+          }}
+        >
+          <View style={styles.titleView}>
+            <Text style={[styles.text, { color: color.white }]}>
+              {
+                getLanguage(global.language).Map_Main_Menu
+                  .MAP_AI_POSE_ESTIMATION_LOOK
+              }
+            </Text>
+          </View>
+
+          <FlatList
+            style={{
+              height: '100%',
+            }}
+            data={this.state.poseData}
+            renderItem={this.renderItem}
+            keyExtractor={this._keyExtractor}
+            ItemSeparatorComponent={this._renderItemSeparatorComponent}
+          />
+        </View>
+      </View>
+    )
+  }
+
   render() {
     return (
-      <Container
-        ref={ref => (this.Container = ref)}
-        headerProps={{
-          title: getLanguage(global.language).Map_Main_Menu
-            .MAP_AI_POSE_ESTIMATION,
-          navigation: this.props.navigation,
-          type: 'fix',
-          backAction: this.back,
-        }}
-        bottomProps={{ type: 'fix' }}
-      >
-        <SMAIPoseEstimationView style={{ flex: 1 }} />
-        {this.state.associationMap &&
-          !this.state.poseOverLook &&
-          this.renderPoseTitle()}
+      <View style={[styles.container, { backgroundColor: color.transView }]}>
+        <View style={[styles.container, { backgroundColor: color.transView }]}>
+          <View
+            style={{
+              position: 'absolute',
+              height: '100%',
+              width: '100%',
+            }}
+          >
+            <Container
+              ref={ref => (this.Container = ref)}
+              headerProps={{
+                title: getLanguage(global.language).Map_Main_Menu
+                  .MAP_AI_POSE_ESTIMATION,
+                navigation: this.props.navigation,
+                type: 'fix',
+                backAction: this.back,
+              }}
+              bottomProps={{ type: 'fix' }}
+            >
+              <SMAIPoseEstimationView style={{ flex: 1 }} />
+              {this.state.associationMap &&
+                !this.state.poseOverLook &&
+                this.renderPoseTitle()}
+              {/* {this.renderBottomBtns()} */}
+            </Container>
+          </View>
+          {this.state.selectPoseMode && this.renderModelList()}
+        </View>
         {this.renderBottomBtns()}
-      </Container>
+      </View>
     )
   }
 }

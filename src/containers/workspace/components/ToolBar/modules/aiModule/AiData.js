a2717d004a73bef9aa846441711138c4a0506773
DragonYang <dragonyang0917@gmail.com>
2020-10-19 13:56:35 +0800

    修复AiData中getData(type)添加type类型判断，防止AR分析目标识别和目标聚合底部Toolbar获取数据导致界面错误
    作者：yangsl
    本领域专家：xiezy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/containers/workspace/components/ToolBar/modules/aiModule/AiData.js
+++ src/containers/workspace/components/ToolBar/modules/aiModule/AiData.js
@@ -1,86 +1,92 @@
 import { getThemeAssets } from '../../../../../../assets'
 import { getLanguage } from '../../../../../../language'
+import { ConstToolType } from '../../../../../../constants'
 import AiActions from './AiActions'
 import ToolbarModule from '../ToolbarModule'
 import { Platform } from 'react-native'
 
-function getData() {
+function getData(type) {
   const _params = ToolbarModule.getParams()
   const buttons = []
-  const data = [
-    {
-      // 目标采集
-      key: getLanguage(global.language).Map_Main_Menu
-        .MAP_AR_AI_ASSISTANT_TARGET_COLLECT,
-      title: getLanguage(global.language).Map_Main_Menu
-        .MAP_AR_AI_ASSISTANT_TARGET_COLLECT,
-      action: AiActions.aiDetect,
-      size: 'large',
-      image: getThemeAssets().ar.functiontoolbar.rightbar_ai_collect_light,
-    },
-    {
-      // 目标分类
-      key: 'aiClassify',
-      title: getLanguage(global.language).Map_Main_Menu
-        .MAP_AR_AI_ASSISTANT_CLASSIFY,
-      action: AiActions.aiClassify,
-      size: 'large',
-      image: getThemeAssets().ar.functiontoolbar.rightbar_ai_classify_light,
-    },
-    {
-      // 态势采集
-      key: getLanguage(global.language).Map_Main_Menu
-        .MAP_AR_AI_ASSISTANT_AGGREGATE_COLLECT,
-      title: getLanguage(global.language).Map_Main_Menu
-        .MAP_AR_AI_ASSISTANT_AGGREGATE_COLLECT,
-      action: AiActions.polymerizeCollect,
-      size: 'large',
-      image: getThemeAssets().ar.functiontoolbar
-        .rightbar_ai_aggregate_collect_light,
-    },
-    {
-      // 违章采集
-      key: getLanguage(global.language).Map_Main_Menu
-        .MAP_AR_AI_ASSISTANT_VIOLATION_COLLECT,
-      title: getLanguage(global.language).Map_Main_Menu
-        .MAP_AR_AI_ASSISTANT_VIOLATION_COLLECT,
-      action: AiActions.illegallyParkCollect,
-      size: 'large',
-      image: getThemeAssets().ar.functiontoolbar.rightbar_ai_violation_light,
-    },
-    // {
-    //   //路面采集
-    //   key: getLanguage(global.language).Map_Main_Menu
-    //     .MAP_AR_AI_ASSISTANT_ROAD_COLLECT,
-    //   title: getLanguage(global.language).Map_Main_Menu
-    //     .MAP_AR_AI_ASSISTANT_ROAD_COLLECT,
-    //   // action:openMap,
-    //   size: 'large',
-    //   image: getThemeAssets().ar.icon_ar,
-    // },
-  ]
-  if (Platform.OS === 'android') {
-    if (_params.laboratory.poseEstimation) {
-      data.push({
-        // 人体姿态
-        key: getLanguage(global.language).Map_Main_Menu.MAP_AI_POSE_ESTIMATION,
-        title: getLanguage(global.language).Map_Main_Menu.MAP_AI_POSE_ESTIMATION,
-        action: AiActions.poseEstimation,
-        size: 'large',
-        image: getThemeAssets().ar.functiontoolbar.ar_bodyposture,
-      })
-    }
-    if (_params.laboratory.gestureBone) {
-      data.push({
-        // 手势骨骼
-        key: getLanguage(global.language).Map_Main_Menu.MAP_AI_GESTURE_BONE,
-        title: getLanguage(global.language).Map_Main_Menu.MAP_AI_GESTURE_BONE,
-        action: AiActions.gestureBone,
-        size: 'large',
-        // image: getThemeAssets().ar.functiontoolbar.ar_bodyposture,
-        image: require('../../../../../../assets/mapTools/icon_select_by_rectangle.png'),
-      })
-    }
+  let data = []
+  // if (type === ConstToolType.SM_MAP_AR_ANALYSIS)
+  switch (type) {
+    case ConstToolType.SM_MAP_AR_ANALYSIS:
+      data = [
+        {
+          // 目标采集
+          key: getLanguage(global.language).Map_Main_Menu
+            .MAP_AR_AI_ASSISTANT_TARGET_COLLECT,
+          title: getLanguage(global.language).Map_Main_Menu
+            .MAP_AR_AI_ASSISTANT_TARGET_COLLECT,
+          action: AiActions.aiDetect,
+          size: 'large',
+          image: getThemeAssets().ar.functiontoolbar.rightbar_ai_collect_light,
+        },
+        {
+          // 目标分类
+          key: 'aiClassify',
+          title: getLanguage(global.language).Map_Main_Menu
+            .MAP_AR_AI_ASSISTANT_CLASSIFY,
+          action: AiActions.aiClassify,
+          size: 'large',
+          image: getThemeAssets().ar.functiontoolbar.rightbar_ai_classify_light,
+        },
+        {
+          // 态势采集
+          key: getLanguage(global.language).Map_Main_Menu
+            .MAP_AR_AI_ASSISTANT_AGGREGATE_COLLECT,
+          title: getLanguage(global.language).Map_Main_Menu
+            .MAP_AR_AI_ASSISTANT_AGGREGATE_COLLECT,
+          action: AiActions.polymerizeCollect,
+          size: 'large',
+          image: getThemeAssets().ar.functiontoolbar
+            .rightbar_ai_aggregate_collect_light,
+        },
+        {
+          // 违章采集
+          key: getLanguage(global.language).Map_Main_Menu
+            .MAP_AR_AI_ASSISTANT_VIOLATION_COLLECT,
+          title: getLanguage(global.language).Map_Main_Menu
+            .MAP_AR_AI_ASSISTANT_VIOLATION_COLLECT,
+          action: AiActions.illegallyParkCollect,
+          size: 'large',
+          image: getThemeAssets().ar.functiontoolbar.rightbar_ai_violation_light,
+        },
+        // {
+        //   //路面采集
+        //   key: getLanguage(global.language).Map_Main_Menu
+        //     .MAP_AR_AI_ASSISTANT_ROAD_COLLECT,
+        //   title: getLanguage(global.language).Map_Main_Menu
+        //     .MAP_AR_AI_ASSISTANT_ROAD_COLLECT,
+        //   // action:openMap,
+        //   size: 'large',
+        //   image: getThemeAssets().ar.icon_ar,
+        // },
+      ]
+      if (Platform.OS === 'android') {
+        if (_params.laboratory.poseEstimation) {
+          data.push({
+            // 人体姿态
+            key: getLanguage(global.language).Map_Main_Menu.MAP_AI_POSE_ESTIMATION,
+            title: getLanguage(global.language).Map_Main_Menu.MAP_AI_POSE_ESTIMATION,
+            action: AiActions.poseEstimation,
+            size: 'large',
+            image: getThemeAssets().ar.functiontoolbar.ar_bodyposture,
+          })
+        }
+        if (_params.laboratory.gestureBone) {
+          data.push({
+            // 手势骨骼
+            key: getLanguage(global.language).Map_Main_Menu.MAP_AI_GESTURE_BONE,
+            title: getLanguage(global.language).Map_Main_Menu.MAP_AI_GESTURE_BONE,
+            action: AiActions.gestureBone,
+            size: 'large',
+            // image: getThemeAssets().ar.functiontoolbar.ar_bodyposture,
+            image: require('../../../../../../assets/mapTools/icon_select_by_rectangle.png'),
+          })
+        }
+      }
   }
   return { data, buttons }
 }

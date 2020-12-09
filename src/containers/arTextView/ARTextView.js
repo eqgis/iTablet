9d51518b1a7579a5f9a13b62b8f3b79e8fe00305
xiezhiyan <xiezhiyanfighting@163.com>
2020-12-09 16:45:52 +0800

    jiakai_AR投放中功能退出崩溃问题
    jiakai_AR投放中功能退出崩溃问题
    作者：jiakai
    同行：yangshanglong
    本领域专家：yangshanglong
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/containers/arTextView/ARTextView.js
+++ src/containers/arTextView/ARTextView.js
@@ -21,6 +21,7 @@ export default class ARTextView extends React.Component {
     super(props)
     let params = this.props.navigation.state.params || {}
     this.point = params.point
+    this.clickAble = true // 防止重复点击
   }
 
   // eslint-disable-next-line
@@ -46,10 +47,13 @@ export default class ARTextView extends React.Component {
   componentWillUnmount() {}
 
   back = () => {
-    SARText.onDestroy()
-    NavigationService.goBack()
-    GLOBAL.toolBox && GLOBAL.toolBox.removeAIDetect(false)
-    GLOBAL.toolBox.switchAr()
+    if (this.clickAble) {
+      this.clickAble = false
+      SARText.onDestroy()
+      NavigationService.goBack('ARTextView')
+      GLOBAL.toolBox && GLOBAL.toolBox.removeAIDetect(false)
+      GLOBAL.toolBox.switchAr()
+    }
   }
 
   // renderBottom = () => {

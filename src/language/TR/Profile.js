3fe1e6e97ad077ee5ebb3dd2f166db92d5b2b682
xiezhiyan <xiezhiyanfighting@163.com>
2020-12-14 17:16:29 +0800

    jiakai_AR测量自动捕捉功能，AR网页投放属性栏不显示问题
    jiakai_AR测量自动捕捉功能，AR网页投放属性栏不显示问题
    作者：jiakai
    同行：yangshanglong
    本领域专家：yangshanglong
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/language/TR/Profile.js
+++ src/language/TR/Profile.js
@@ -339,6 +339,7 @@ const Profile = {
   MAP_AR_DATUM_SETTING: 'Kurulum',
   X_COORDINATE: 'X Koordinatı',
   Y_COORDINATE: 'Y Koordinatı',
+  MAP_AR_DATUM_AUTO_CATCH: 'Auto Catch',//待翻译
 
   // ar地图
   COLLECT_SCENE_RENAME: 'Yeniden adlandırma',

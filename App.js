51cdd7476e0ec1b272b7ef829695d996f02c64ea
DragonYang <dragonyang0917@gmail.com>
2021-02-03 15:43:21 +0800

    修复ios启动App第一次登录超时问题
    作者：yangsl
    本领域专家：xiezy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- App.js
+++ App.js
@@ -350,14 +350,17 @@ class AppRoot extends Component {
     if (UserType.isOnlineUser(this.props.user.currentUser)) {
       let result
       result = await this.loginOnline()
-      if (result) {
+      if (result === true) {
         result = await FriendListFileHandle.initFriendList(this.props.user.currentUser)
+      } else {
+        // iOS防止第一次登录timeout
+        result = await this.loginOnline()
       }
       // if (result) {
       //   // 初始化协作文件
       //   CoworkFileHandle.initCoworkList(this.props.user.currentUser)
       // }
-      if(result){
+      if(result === true){
         let JSOnlineservice = new OnlineServicesUtils('online')
         //登录后更新用户信息 zhangxt
         let userInfo = await JSOnlineservice.getUserInfo(this.props.user.currentUser.nickname, true)
@@ -465,24 +468,24 @@ class AppRoot extends Component {
       SSpeechRecognizer.init('5b63b509')
     }
     AppState.addEventListener('change', this.handleStateChange)
-      ; (async function () {
-        await this.initDirectories()
-        await this.initUser()
-        SOnlineService.init()
-        // SOnlineService.removeCookie()
-        SIPortalService.init()
-        await this.getVersion()
-        await this.getImportState()
-        await this.addImportExternalDataListener()
-        await this.addGetShareResultListener()
-        await this.openWorkspace()
-        await this.initOrientation()
+    ; (async function () {
+      await this.initDirectories()
+      await this.initUser()
+      SOnlineService.init()
+      // SOnlineService.removeCookie()
+      SIPortalService.init()
+      await this.getVersion()
+      await this.getImportState()
+      await this.addImportExternalDataListener()
+      await this.addGetShareResultListener()
+      await this.openWorkspace()
+      await this.initOrientation()
 
-        // 显示界面，之前的为预加载
-        this.setState({ isInit: true }, () => {
-          this.login()
-        })
-      }).bind(this)()
+      // 显示界面，之前的为预加载
+      this.setState({ isInit: true }, () => {
+        this.login()
+      })
+    }).bind(this)()
 
     GLOBAL.clearMapData = () => {
       this.props.setEditLayer(null) // 清空地图图层中的数据
@@ -573,7 +576,7 @@ class AppRoot extends Component {
   handleStateChange = appState => {
     if (appState === 'active') {
       if (UserType.isOnlineUser(this.props.user.currentUser)) {
-        this.loginOnline()
+        this.login()
         this.reCircleLogin()
       }
       Orientation.getOrientation((e, orientation) => {

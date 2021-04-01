0f560157792034420fa255ce7fec0a8c1bfe9153
Yang Shang Long <dragonyang0917@gmail.com>
2021-04-01 13:42:08 +0800

    修复启动页轮播图，Android物理返回崩溃问题
    作者：yangsl
    本领域专家：xiezy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- App.js
+++ App.js
@@ -201,7 +201,8 @@ class AppRoot extends Component {
       import: null,
       isInit: false,
       /**判断是否显示APP启动引导页，根据launchGuideVersion是否一致和是否有启动页 */
-      showLaunchGuide: config.launchGuideVersion > (this.props.appConfig.launchGuideVersion || '0') && guidePages.length > 0,
+      // showLaunchGuide: config.launchGuideVersion > (this.props.appConfig.launchGuideVersion || '0') && guidePages.length > 0,
+      showLaunchGuide: true,
     }
     // this.preLaunchGuideVersion = this.props.appConfig.launchGuideVersion
     this.props.setModules(config) // 设置模块
@@ -551,7 +552,14 @@ class AppRoot extends Component {
   }
 
   back = () => {
-    return BackHandlerUtil.backHandler(this.props.nav, this.props.backActions)
+    if (this.state.showLaunchGuide) {
+      this.setState({
+        showLaunchGuide: false,
+      })
+      return true
+    } else {
+      return BackHandlerUtil.backHandler(this.props.nav, this.props.backActions)
+    }
   }
 
   onInvalidModule = () => {

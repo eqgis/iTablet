36c67015c2594886582db93e8fe618714a93691d
Yang Shang Long <dragonyang0917@gmail.com>
2021-04-01 13:46:38 +0800

    Revert "修复启动页轮播图，Android物理返回崩溃问题"
    This reverts commit 0f560157792034420fa255ce7fec0a8c1bfe9153.

--- App.js
+++ App.js
@@ -201,8 +201,7 @@ class AppRoot extends Component {
       import: null,
       isInit: false,
       /**判断是否显示APP启动引导页，根据launchGuideVersion是否一致和是否有启动页 */
-      // showLaunchGuide: config.launchGuideVersion > (this.props.appConfig.launchGuideVersion || '0') && guidePages.length > 0,
-      showLaunchGuide: true,
+      showLaunchGuide: config.launchGuideVersion > (this.props.appConfig.launchGuideVersion || '0') && guidePages.length > 0,
     }
     // this.preLaunchGuideVersion = this.props.appConfig.launchGuideVersion
     this.props.setModules(config) // 设置模块
@@ -552,14 +551,7 @@ class AppRoot extends Component {
   }
 
   back = () => {
-    if (this.state.showLaunchGuide) {
-      this.setState({
-        showLaunchGuide: false,
-      })
-      return true
-    } else {
-      return BackHandlerUtil.backHandler(this.props.nav, this.props.backActions)
-    }
+    return BackHandlerUtil.backHandler(this.props.nav, this.props.backActions)
   }
 
   onInvalidModule = () => {

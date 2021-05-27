8e9c1a3e6ec3c83c382b7ed5ad4db2f43bf4c8e0
xiezhiyan <xiezhiyanfighting@163.com>
2021-05-27 15:32:02 +0800

    Update layers.js

--- src/redux/models/layers.js
+++ src/redux/models/layers.js
@@ -56,7 +56,7 @@ export const setCurrentAttribute = (
 
 export const setCurrentLayer = (params, cb = () => {}) => async dispatch => {
   if (params && params.path) {
-      SMap.setLayerEditable(params.path, true)
+    await SMap.setLayerEditable(params.path, true)
   }
   await dispatch({
     type: SET_CURRENT_LAYER,

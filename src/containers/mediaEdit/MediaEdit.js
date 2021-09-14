caebf5fdbd31be2c50a5910469f64ef4e23b60ae
xiezhiyan <xiezhiyanfighting@163.com>
2021-09-14 16:35:54 +0800

    jiakai_多媒体属性无内容时点击保存选择删除时未刷新问题
    作者：jiakai
    同行：ysl
    本领域专家：ysl
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/containers/mediaEdit/MediaEdit.js
+++ src/containers/mediaEdit/MediaEdit.js
@@ -47,6 +47,7 @@ export default class MediaEdit extends React.Component {
     this.info = (params && params.info) || {}
     this.layerInfo = params && params.layerInfo
     this.cb = params && params.cb
+    this.gocb = params && params.gocb
     this.backAction = params && params.backAction
     let paths = []
 
@@ -610,6 +611,10 @@ export default class MediaEdit extends React.Component {
           await this.save(this.modifiedData, true)
           SMap.refreshMap()
           NavigationService.goBack('MediaEdit')
+          this.cb && typeof this.cb === 'function' && this.cb()
+          if(GLOBAL.HAVEATTRIBUTE){
+            this.gocb && typeof this.gocb === 'function' && this.gocb()
+          }
         }}
         confirmText={getLanguage(this.props.language).Prompt.DELETE}
         cancelAction={() => {

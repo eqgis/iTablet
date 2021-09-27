5baa8dc36776498ee80972b0eed906035a649875
Shanglong Yang <shanglongyang@Shanglongs-Mac-mini.local>
2021-09-27 17:12:06 +0800

    修复新建属性字段属性名称后确定，提示不能以SS_开头命名
    作者：yangsl
    本领域专家：xiezy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/containers/layerAttribute/pages/layerAttributeAdd/LayerAttributeAdd.js
+++ src/containers/layerAttribute/pages/layerAttributeAdd/LayerAttributeAdd.js
@@ -123,11 +123,11 @@ export default class LayerAttributeAdd extends React.Component {
     let isConfirm = false
     if (!this.state.name || this.state.name === '') {
       Toast.show(getLanguage(GLOBAL.language).Prompt.ENTER_NAME)
-    } else if (this.state.name !== undefined && this.state.name !== '' && this.state.name.toLowerCase().indexOf('ss_')) {
+    } else if (this.state.name !== undefined && this.state.name !== '' && this.state.name.toLowerCase().indexOf('ss_') === 0) {
       Toast.show(getLanguage(GLOBAL.language).Prompt.DEFAULT_NAMING_SS)
     } else if (!this.state.caption || this.state.caption === '') {
       Toast.show(getLanguage(GLOBAL.language).Prompt.ENTER_CAPTION)
-    } else if (this.state.caption !== undefined && this.state.caption !== '' && this.state.caption.toLowerCase().indexOf('ss_')) {
+    } else if (this.state.caption !== undefined && this.state.caption !== '' && this.state.caption.toLowerCase().indexOf('ss_') === 0) {
       Toast.show(getLanguage(GLOBAL.language).Prompt.DEFAULT_NAMING_SS)
     } else if (!this.state.type) {
       Toast.show(getLanguage(GLOBAL.language).Prompt.CHOICE_TYPE)

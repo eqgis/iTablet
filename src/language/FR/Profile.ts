7d02811e45d75c0446277b95890a5c3b0784eace
xiezhiyan <xiezhiyanfighting@163.com>
2021-10-13 14:39:30 +0800

    翻译修改

--- src/language/FR/Profile.ts
+++ src/language/FR/Profile.ts
@@ -124,7 +124,7 @@ const Profile: typeof CN.Profile = {
   MY_SERVICE: 'Service',
   PRIVATE_SERVICE: "Service privé'",
   PUBLIC_SERVICE: 'Service publique',
-  NO_MAP_SERVICE:'Service has no map',// need to translate
+  NO_MAP_SERVICE:'The service does not have a map to display',// need to translate
 
   INVALID_SERVER_ADDRESS: 'Addresse du serveur est invalide',
 

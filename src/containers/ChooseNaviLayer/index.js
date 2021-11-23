1e7c6b293924a43ec53dc053aed619bcd19ee4a0
xiezhiyan <xiezhiyanfighting@163.com>
2021-11-23 15:31:08 +0800

    Create index.js

---
+++ src/containers/ChooseNaviLayer/index.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux'
+import ChooseNaviLayer from './ChooseNaviLayer'
+import { setCurrentLayer ,getLayers} from '../../redux/models/layers'
+
+const mapDispatchToProps = {
+  setCurrentLayer,
+  getLayers,
+}
+
+const mapStateToProps = state => ({
+  user: state.user.toJS(),
+  language: state.setting.toJS().language,
+  device: state.device.toJS().device,
+  currentLayer: state.layers.toJS().currentLayer,
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ChooseNaviLayer)

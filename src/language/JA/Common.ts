5661b9668994631740463bfdb842660556d82ce4
005_SuperMap <xiezhiyanfighting@163.com>
2022-03-07 09:27:13 +0800

    lyx 替换日本语言的翻译

--- src/language/JA/Common.ts
+++ src/language/JA/Common.ts
@@ -1,13 +1,13 @@
 import CN from "../CN"
 
 const Protocol: typeof CN.Protocol = {
-  PROTOCOL: "スーパーマップユーザーサービス協議",
+  PROTOCOL: "SuperMapサービス規約",
   AGREE: "同意",
-  READ_AND_AGREE: "閲読済み、受け取ります。",
+  READ_AND_AGREE: "閲読済み、同意",
   AGAIN:"もう一度確認",
   CONFIRM_EXIT:"アプリケーションを終了",
   REMINDER:"リマインダー",
-  AGREEMENT:"弊社は個人情報の保護を非常に重要視し、SuperMapプライバシーポリシーに厳密に従ってあなたの情報を保護および処理することを約束します。あなたがこのポリシーに同意しない場合、残念ながら、サービスを提供することはできません",
+  AGREEMENT:"弊社は個人情報の保護を非常に重要視し、「個人情報保護方針」に厳密に従ってあなたの個人情報を保護および処理することを約束します。あなたがこのポリシーに同意しない場合、残念ながら、サービスを提供することはできません",
 }
 
 const Common: typeof CN.Common = {
@@ -24,7 +24,7 @@ const Common: typeof CN.Common = {
   ADD: "追加",
   NONE: "なし",
 
-  DELETE_CURRENT_OBJ_CONFIRM: "現在オブジェクトを削除しますか?",
+  DELETE_CURRENT_OBJ_CONFIRM: "現在のオブジェクトを削除しますか?",
   NO_SELECTED_OBJ: "選択中のオブジェクトはありません",
 
   CURRENT: '現在',

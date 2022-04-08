aafaaa20c86a96aa26fa78361ef98da25998dc4f
005_SuperMap <xiezhiyanfighting@163.com>
2022-04-08 18:27:51 +0800

    lyx 修改定位授权在定位初始化之后的问题
    作者：lyx
    同行：xiezy
    本领域专家：xiezy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/containers/tabs/Home/components/ModuleList.js
+++ src/containers/tabs/Home/components/ModuleList.js
@@ -5,7 +5,7 @@ import { scaleSize, Toast, FetchUtils } from '../../../../utils'
 import { Module } from '../../../../class'
 import { color } from '../../../../styles'
 import { FileTools } from '../../../../native'
-import { SMap ,SMeasureView } from 'imobile_for_reactnative'
+import { SMap ,SMeasureView, SLocation } from 'imobile_for_reactnative'
 import {
   downloadFile,
   deleteDownloadFile,
@@ -318,6 +318,10 @@ class ModuleList extends Component {
         if (isAllGranted && permisson11) {
           SMap.setPermisson(true)
           // this.init()
+
+          // 重新设置权限后，重新打开定位
+          await SLocation.openGPS()
+
         } else {
           this.props.itemAction()
           item.key !== ChunkType.APPLET_ADD && item.spin && item.spin(false) // 停止转圈

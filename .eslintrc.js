a391bebafc1e8358a72a49199f0c6c8b43fe6851
zhangxt <zhangxintao@supermap.com>
2022-05-26 12:35:07 +0800

    更新语法检测规则
    作者：zhangxt
    同行：xiezhy
    本领域专家：xiezhy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    'max-len': 'off',
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    'semi': ['error', 'never'],
+    'no-trailing-spaces': ['error'],
+    'react/display-name': ['off'],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-var-requires": "off"
+  }
+}

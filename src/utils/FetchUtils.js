d5e62a7ad233d9bd373e03a1735120abced1462b
005_SuperMap <xiezhiyanfighting@163.com>
2022-06-20 08:44:23 +0800

    lyx 解决使用在线路网分析，地图上导航路线未显示的问题
    作者：lyx
    同行：ysl
    本领域专家：xzy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/utils/FetchUtils.js
+++ src/utils/FetchUtils.js
@@ -106,7 +106,7 @@ export default class FetchUtils {
   static routeAnalyst = async (startX, startY, endX, endY) => {
     let data
     try {
-      let params = `{startPoint:{"x":${startX},"y":${startY}},endPoint:{"x":${endX},"y":${endY}},routeType:MINLENGTH,to:910111}`
+      let params = `{startPoint:{"x":${startX},"y":${startY}},endPoint:{"x":${endX},"y":${endY}},routeType:MINLENGTH}` // ,to:910111 加to表示要将所有坐标都转为目的坐标系的坐标
       let url = `https://www.supermapol.com/iserver/services/navigation/rest/navigationanalyst/China/pathanalystresults.json?pathAnalystParameters=[${params}]&key=fvV2osxwuZWlY0wJb8FEb2i5`
       data = await FetchUtils.getObjJson(url)
     } catch (e) {

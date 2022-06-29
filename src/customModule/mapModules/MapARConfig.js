806b1718c138d450ef5d79b30f3c5d9ab6a80691
DragonYang <shanglongyang@Shanglongs-Mac-mini.local>
2022-06-29 16:01:53 +0800

    修复横屏进入AR模块界面有黑边（刘海屏）
    作者：yangsl
    本领域专家：xiezy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- src/customModule/mapModules/MapARConfig.js
+++ src/customModule/mapModules/MapARConfig.js
@@ -127,8 +127,21 @@ export default class MapARConfig extends Module {
           return false
         }
         SMap.setDynamicviewsetVisible(false)
-        screen.lockToPortrait()
+        // 竖屏时,锁定竖屏
+        if (screen.getOrientation().indexOf('LANDSCAPE') < 0) {
+          screen.lockToPortrait()
+        }
         return isAvailable
+      },
+      afterAction: async () => {
+        // 横屏时,等跳转后,再锁定竖屏
+        if (screen.getOrientation().indexOf('LANDSCAPE') >= 0) {
+          let timer = setTimeout(() => {
+            screen.lockToPortrait()
+            clearTimeout(timer)
+          }, 100)
+        }
+        return true
       },
     })
   }

a77467790e15421c1394e37f927ab2a550d70f9a
005_SuperMap <xiezhiyanfighting@163.com>
2022-11-17 20:17:02 +0800

    lyx 进入模块，先不显示扫描界面，等待三秒后自动开启或等ar提示完成开启
    作者：lyx
    同行：zxt
    本领域专家：xiezhy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- applets/exhibition/src/toolbarModules/exhibition/views/DoctorCC.tsx
+++ applets/exhibition/src/toolbarModules/exhibition/views/DoctorCC.tsx
@@ -172,12 +172,14 @@ class DoctorCC extends Component<Props, State> {
 
   scaleValue = 100
   show = true
+  /** 第一次显示扫描界面是否完成 */
+  scanFirstShow = false
 
   constructor(props: Props) {
     super(props)
     this.state = {
       showScan: true,
-      isScan: true,
+      isScan: false,
       selectType: 'null',
       animations: [],
       selectAnimationKey: -1,
@@ -221,10 +223,20 @@ class DoctorCC extends Component<Props, State> {
 
     this.getDoctorData()
     await this.openDoctorARMap()
-    if(this.state.showScan && this.state.isScan) {
-      // 启用增强定位
-      SARMap.setAREnhancePosition()
-    }
+
+    const scanShowTimer = setTimeout(() => {
+      if(!this.scanFirstShow) {
+        if(this.state.showScan && !this.state.isScan) {
+          // 启用增强定位
+          SARMap.setAREnhancePosition()
+        }
+        this.scanFirstShow = true
+        this.setState({
+          isScan: true,
+        })
+      }
+      clearTimeout(scanShowTimer)
+    }, 3000)
 
     this.listeners = SARMap.addMeasureStatusListeners({
       addListener: async result => {
@@ -233,6 +245,7 @@ class DoctorCC extends Component<Props, State> {
             // 启用增强定位
             SARMap.setAREnhancePosition()
           }
+          this.scanFirstShow = true
           this.setState({
             isScan: true,
           })

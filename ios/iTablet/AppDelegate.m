059e9477ff5d25c3554a8de4548d05e3fc593c0e
Yang Shang Long <dragonyang0917@gmail.com>
2022-12-28 10:30:10 +0800

    Update AppDelegate.m
    作者：yangsl
    本领域专家：xiezy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- ios/iTablet/AppDelegate.m
+++ ios/iTablet/AppDelegate.m
@@ -143,7 +143,7 @@ static NSString* g_sampleCodeName = @"#";;
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"iTablet"
+                                                   moduleName:@"base"
                                             initialProperties:nil];
   
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];

576a0d6faeb4369ccaa8981f391fa9c60a2dcef6
xiezhiyan <xiezhiyanfighting@163.com>
2023-01-09 15:53:43 +0800

    jiakai_扫描界面退出没有清楚定时器问题
    作者：jiakai
    同行：ysl
    本领域专家：xzy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- applets/exhibition/src/toolbarModules/exhibition/components/ScanWrap.tsx
+++ applets/exhibition/src/toolbarModules/exhibition/components/ScanWrap.tsx
@@ -19,9 +19,8 @@ interface State {
   hintText: string
 }
 
-
 class ScanWrap extends React.Component<Props, State> {
-
+  timer:any
   scanRef: Scan | null = null
 
   constructor(props: Props) {
@@ -32,12 +31,16 @@ class ScanWrap extends React.Component<Props, State> {
   }
 
   componentDidMount = (): void => {
-    const timer = setTimeout(() => {
+    this.timer = setTimeout(() => {
       this.setState({
         hintText: '请调整距离将二维码放入扫描圈内',
       })
-      clearTimeout(timer)
-    },5000)
+      clearTimeout(this.timer)
+    }, 5000)
+  }
+
+  componentWillUnmount(): void {
+    clearTimeout(this.timer)
   }
 
   renderScan = () => {

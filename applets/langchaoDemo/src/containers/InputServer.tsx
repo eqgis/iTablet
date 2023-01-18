2d2c9896259ba232c8a16e82b8fcc339b589d236
005_SuperMap <xiezhiyanfighting@163.com>
2023-01-18 16:40:41 +0800

    lyx 临时恢复服务设置里直接设置当前用户的界面
    作者：lyx
    同行：zxt
    本领域专家：xiezhy
    相应测试案例：不需要添加
    代码审查时间：0.1小时

--- applets/langchaoDemo/src/containers/InputServer.tsx
+++ applets/langchaoDemo/src/containers/InputServer.tsx
@@ -26,9 +26,9 @@ interface Props extends ReduxProps {
 
 interface State {
 	serverUrl: string,
-  // userId:string,
-  // userName: string,
-  // sysOrgId: string,
+  userId:string,
+  userName: string,
+  sysOrgId: string,
 }
 
 class InputServer extends Component<Props, State> {
@@ -39,9 +39,9 @@ class InputServer extends Component<Props, State> {
     super(props)
     this.state = {
       serverUrl: props.serverIP || "",
-      // userId: props.userId || "",
-      // userName: props.userName || "",
-      // sysOrgId: props.departmentId || "",
+      userId: props.userId || "",
+      userName: props.userName || "",
+      sysOrgId: props.departmentId || "",
     }
   }
 
@@ -59,9 +59,9 @@ class InputServer extends Component<Props, State> {
     // setUserId(this.state.userId)
     // setUserName(this.state.userName)
     // setSysOrgid(this.state.sysOrgId)
-    // await this.props.setServerUserId(this.state.userId)
-    // await this.props.setServerUserName(this.state.userName)
-    // await this.props.setServerDepartmentId(this.state.sysOrgId)
+    await this.props.setServerUserId(this.state.userId)
+    await this.props.setServerUserName(this.state.userName)
+    await this.props.setServerDepartmentId(this.state.sysOrgId)
     await getToken()
 
     // const date = new Date()
@@ -146,7 +146,7 @@ class InputServer extends Component<Props, State> {
           </TouchableOpacity>
         </View>
 
-        {/* <View style={[{
+        <View style={[{
           width: '100%',
           height: dp(40),
           backgroundColor: "#f3f3f3",
@@ -243,7 +243,7 @@ class InputServer extends Component<Props, State> {
               source={getPublicAssets().common.icon_close}
             />
           </TouchableOpacity>
-        </View> */}
+        </View>
       </View>
     )
   }

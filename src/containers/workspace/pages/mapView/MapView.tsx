23803d4c48662c7c9b24fe4558e3e77a9801e64e
005_SuperMap <xiezhiyanfighting@163.com>
2023-03-06 16:16:43 +0800

    Revert "lyx 临时提交  显示校准次数"
    This reverts commit ae6a90e27a419c082b9fe27a7eebcf64bc0514cb.

--- src/containers/workspace/pages/mapView/MapView.tsx
+++ src/containers/workspace/pages/mapView/MapView.tsx
@@ -122,8 +122,6 @@ import {
   Dimensions,
   DeviceEventEmitter,
   PixelRatio,
-  NativeModules,
-  NativeEventEmitter,
 } from 'react-native'
 import { getLanguage } from '../../../../language/index'
 import styles from './styles'
@@ -153,10 +151,6 @@ import PositionStateView from '../../components/PositionStateView'
 import { SNavigationInner } from 'imobile_for_reactnative/NativeModule/interfaces/navigation/SNavigationInner'
 import { addOutdoorStartEndGuideLine } from '../../components/NavigationView/NavigationView'
 import IndoorGPSCollector from '../../components/ToolBar/modules/toolModule/IndoorGPSCollector'
-
-
-const nativeLocation = NativeModules.SLocation
-const eventEmitter = new NativeEventEmitter(nativeLocation)
 
 global.markerTag = 118082
 
@@ -394,7 +388,6 @@ export default class MapView extends React.Component {
       showPoiSearch: false,
       showNavigation: false,
       isFull: true,
-      ntripInfo: "",
     }
     // this.props.setDatumPoint(global.Type === ChunkType.MAP_AR ? true : false)
     this.props.setDatumPoint(false)
@@ -557,7 +550,6 @@ export default class MapView extends React.Component {
   }
 
   async componentDidMount() {
-    this.getNtripInfo()
     AppEvent.addListener('on_exit_ar_map_module', this.back)
     if (!global.isLicenseValid) {
       const licenseStatus = await SData.getEnvironmentStatus()
@@ -5331,37 +5323,6 @@ export default class MapView extends React.Component {
     }
   }
 
-
-  getNtripInfo = () => {
-    eventEmitter.addListener("NtripInfo", (text: string) => {
-      this.setState({
-        ntripInfo: text,
-      })
-    })
-  }
-
-
-  renderNtripInfo = () => {
-    return (
-      <View
-        style={[
-          {
-            position: 'absolute',
-            width: scaleSize(300),
-            height: scaleSize(40),
-            top: scaleSize(140),
-            left: scaleSize(20),
-            backgroundColor: 'rgba(0,0,0,.5)',
-          },
-        ]}
-      >
-        <Text
-          style={[styles.bottomInfoText]}
-        >{this.state.ntripInfo}</Text>
-      </View>
-    )
-  }
-
   renderContainer = () => {
     const width = this.px(screen.getScreenWidth())
     return (
@@ -5598,7 +5559,6 @@ export default class MapView extends React.Component {
         {this.renderCustomInputDialog()}
         {this.renderCustomAlertDialog()}
         {this.getInfoText()}
-        {this.renderNtripInfo()}
 
         <Toolbar
           navigation={this.props.navigation}
